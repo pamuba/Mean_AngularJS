b9afd72f1a8305610a151ca0ee4e2648ab9a4fa2
pamuba <pamuba@users.noreply.github.com>
2020-07-01 01:02:52 +0530

    17th

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+var mongojs = require('mongojs');
+
+var db = mongojs('contactlist',['contactlist'])
 
 // app.get('/', function(req, res){
 //     res.send("Hello world from server.js")
@@ -12,25 +15,10 @@ app.use(express.static(__dirname + '/public'))
 app.get('/contactlist', function(req, res){
     console.log("Express received a GET request")
 
-    person1 = {
-        name:"Jack",
-        email:"jack@gmail.com",
-        number:'(111) 111-1111'
-    };
-    person2 = {
-        name:"Emily",
-        email:"emily@gmail.com",
-        number:'(222) 222-2222'
-    };
-    person3 = {
-        name:"John",
-        email:"john@gmail.com",
-        number:'(333) 333-3333'
-    };
-
-    var contactlist = [person1, person2, person3]
-
-    res.json(contactlist);
+    db.contactlist.find(function(err, docs){
+        console.log(docs)
+        res.json(docs)
+    })
 })
 
 app.listen(3000);
