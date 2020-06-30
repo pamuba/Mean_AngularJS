fdc7d171003ff4b255817786860ac27837d02436
pamuba <pamuba@users.noreply.github.com>
2020-07-01 00:20:10 +0530

    10th

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res){
-    res.send("Hello world from server.js")
-})
+// app.get('/', function(req, res){
+//     res.send("Hello world from server.js")
+// })
+
+//html, css, js
+app.use(express.static(__dirname + '/public'))
 
 app.listen(3000);
 console.log("Server running on port 3000")
+
