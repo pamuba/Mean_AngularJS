d3f6ef5c0f94dc8d5a8a2d8c3168d425fa789b65
pamuba <pamuba@users.noreply.github.com>
2020-07-01 00:42:27 +0530

    15th

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,24 +2,11 @@ function AppCtrl($scope, $http){
     console.log("Hello world from controller");
 
     //route to get the data from 
-    $http.get('/contactlist')
+    $http.get('/contactlist').success(function(response){
+        console.log("Angular recevived the requested data")
+        $scope.contactlist = response;
+    })
 
-    person1 = {
-        name:"Jack",
-        email:"jack@gmail.com",
-        number:'(111) 111-1111'
-    };
-    person2 = {
-        name:"Emily",
-        email:"emily@gmail.com",
-        number:'(222) 222-2222'
-    };
-    person3 = {
-        name:"John",
-        email:"john@gmail.com",
-        number:'(333) 333-3333'
-    };
-
-    var contactlist = [person1, person2, person3]
-    $scope.contactlist = contactlist;
+   
+    
 }
--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/contactlist', function(req, res){
     console.log("Express received a GET request")
+
+    person1 = {
+        name:"Jack",
+        email:"jack@gmail.com",
+        number:'(111) 111-1111'
+    };
+    person2 = {
+        name:"Emily",
+        email:"emily@gmail.com",
+        number:'(222) 222-2222'
+    };
+    person3 = {
+        name:"John",
+        email:"john@gmail.com",
+        number:'(333) 333-3333'
+    };
+
+    var contactlist = [person1, person2, person3]
+
+    res.json(contactlist);
 })
 
 app.listen(3000);
