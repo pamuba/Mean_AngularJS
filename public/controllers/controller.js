e8bf5608743496abb3591d399a6690ff86e5a100
pamuba <pamuba@users.noreply.github.com>
2020-07-02 23:23:26 +0530

    30th

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,16 +1,31 @@
 function AppCtrl($scope, $http){
     console.log("Hello world from controller");
 
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            console.log("Angular recevived the requested data")
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    }
+
+    refresh();
     //route to get the data from 
-    $http.get('/contactlist').success(function(response){
-        console.log("Angular recevived the requested data")
-        $scope.contactlist = response;
-    });
+  
 
     $scope.addContact = function(){
         console.log($scope.contact)
         $http.post('/contactlist', $scope.contact).success(function(response){
-            console.log(response)
+            console.log(response);
+            refresh();
         })
+    };
+
+    $scope.remove = function(id){
+        console.log(id);
+        $http.delete('/contactlist/'+id).success(function(response){
+            refresh();
+        });
     }
+
 }
--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.post('/contactlist', function(req, res){
     })
 })
 
+
+app.delete('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+})
 app.listen(3000);
 console.log("Server running on port 3000")
 
